32ed7bc05be896f6fb424d9bf17a4a6bb8162bf0
saya-git6 <saya.june.6020@gmail.com>
2025-10-07 13:40:04 +0900

    Initial Commit

---
+++ Lesson/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson
+//
+//  Created by 堀之内清華 on 2025/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson/LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonApp.swift
+//  Lesson
+//
+//  Created by 堀之内清華 on 2025/10/07.
+//
+
+import SwiftUI
+
+@main
+struct LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
